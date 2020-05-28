668e0627bd7876dbc344da532f3d6bc88f956e84
Michael Curry <michael.curry@thebeansgroup.com>
2020-05-29 00:24:21 +0100

    Add speakeasy v0.0.1

---
+++ speakeasy/precache-manifest.3d283b64f6bb788b1fbff0151f2fd6c1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2f59b8165e60f893f80",
+    "url": "/static/js/app.a1521b82.chunk.js"
+  },
+  {
+    "revision": "e91487e8c8aa62ce91ce",
+    "url": "/static/js/runtime~app.34c76111.js"
+  },
+  {
+    "revision": "0ff1ae86e804d92fe022",
+    "url": "/static/js/2.4098015c.chunk.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "b3ff6d8d8f703989d98dfa3ef53fcbc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f43dd7032ede6a9f3a17e10f1c1c621",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  }
+];
